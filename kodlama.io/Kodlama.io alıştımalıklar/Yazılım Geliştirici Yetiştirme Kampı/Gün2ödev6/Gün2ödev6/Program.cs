48e70709f3b106bab38e6de619fca9acdaa76ee8
Erol Dalkılıç <eroldalg@gmail.com>
2022-05-07 14:02:17 +0300

    being added

---
+++ kodlama.io/Kodlama.io alıştımalıklar/Yazılım Geliştirici Yetiştirme Kampı/Gün2ödev6/Gün2ödev6/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Product product = new Product();
+        product.Name = "Sherlock Holmes";
+        product.Price = 230;
+        product.isAvaible = true;
+
+        Product product1 = new Product();
+        product1.Name = "Sapıens";
+        product1.Price = 70;
+        product1.isAvaible = false;
+
+        Product product2 = new Product();
+        product2.Name = "Göl Çocukları";
+        product2.Price = 130;
+        product2.isAvaible = true;
+
+        Product product3 = new Product();
+        product3.Name = "Stranç";
+        product3.Price = 160;
+        product3.isAvaible = true;
+
+        Product[] products = new Product[] { product, product1, product2, product3 };
+
+        Console.WriteLine("FOREACH DÖNGÜSÜ İLE");
+        foreach (Product item in products)
+        {
+            Console.WriteLine("Kitap ADI : " + item.Name + " / " + " Kitap FİYATI : " + item.Price + " / " + " Stok DURUMU : " + item.isAvaible);
+        }
+
+        Console.WriteLine("FOR DÖNGÜSÜ İLE");
+        for (int i = 0; i < products.Length; i++)
+        {
+            Console.WriteLine("Kitap ADI : " + products[i].Name + " / " + " Kitap FİYATI : " + " " + products[i].Price + " / " + " Stok DURUMU : " + " " + products[i].isAvaible);
+        }
+
+        Console.WriteLine("WHİLE DÖNGÜSÜ İLE");
+        int x = 0;
+        while (x < products.Length)
+        {
+            Console.WriteLine("Kitap ADI : " + products[x].Name +" / "+ " Kitap FİYATI : "  + products[x].Price + " / " + " Stok DURUMU : " + " " + products[x].isAvaible);
+            x++;
+        }
+    }
+}
+
+class Product
+{
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public bool isAvaible { get; set; }
+}
+
+
+
+
+
---
+++ kodlama.io/kamplar/C# dersleri/MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,127 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Degiskenler();
+
+            Vatandas vatandas1 = new Vatandas();
+
+            SelamVer(isim:"Erol");
+            SelamVer(isim: "Engin");
+            SelamVer(isim: "Ayşe");
+            SelamVer();
+
+            int sonuc = Topla(6,58);
+
+            //Diziler /Arrays
+
+            string ogrenci1 = "Engin";
+            string ogrenci2 = "Kerem";
+            string ogrenci3 = "Erol";
+
+            //Console.WriteLine(ogrenci1);
+            //Console.WriteLine(ogrenci2);
+            //Console.WriteLine(ogrenci3);
+            
+            string[] ogrenciler = new string[3];
+            ogrenciler[0] = "Engin";
+            ogrenciler[1] = "Kerem";
+            ogrenciler[2] = "Erol";
+
+            ogrenciler = new string[4];
+            ogrenciler[3] = "İlker";
+
+            for (int i = 0; i < ogrenciler.Length; i++)
+            {
+                Console.WriteLine(ogrenciler[i]);
+            }
+
+
+            string[] sehirler1 = new[] { "Ankara", "İstanbul", "izmir" };
+            string[] sehirler2 = new[] { "Bursa", "Antalya", "Diyarbakır" };
+
+            sehirler2 = sehirler1;
+            sehirler1[0] = "Adana";
+            Console.WriteLine(sehirler2[0]);
+
+            Person person1 = new Person();
+            person1.FirstName = "Engin";
+            person1.LastName = "Demiroğ";
+            person1.DateOfBirthYear = 1985;
+            person1.NationalIdentity = 123;
+
+            Person person2 = new Person();
+            person2.FirstName = "Murat";
+
+            
+
+            foreach (string sehir in sehirler1)
+            {
+                Console.WriteLine(sehir);
+            }
+
+            //MyList
+            List<string> yeniSehirler1 = new List<string> { "Ankara 1", "İstanbul 1", "izmir 1" };
+            yeniSehirler1.Add(item: "Adana 1");
+            foreach (var sehir in yeniSehirler1)
+            {
+                Console.WriteLine(sehir);
+            }
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1); 
+
+
+            Console.ReadLine();
+        }
+
+        //resharper
+        static void SelamVer(string isim="isimsiz")
+        {
+            Console.WriteLine("Merhaba " + isim);
+        }
+
+        static int Topla(int sayi1 = 5, int sayi2 = 10)
+        {
+            int sonuc= sayi1 + sayi2;
+            Console.WriteLine("Toplam : " + sonuc.ToString());
+            return sonuc;
+        }
+
+        private static void Degiskenler()
+        {
+            string mesaj = "Merhaba";
+            double tutar = 100000; //db den gelir
+            int sayi = 100;
+            bool girişYapmişMi = false;
+
+            string ad = "Erol";
+            string soyad = "Dalkılıç";
+            int dogumYili = 2003;
+            long tcNo = 12345678910;
+
+            Console.WriteLine(tutar * 1.18);
+
+            Console.WriteLine(tutar * 1.18);
+        }
+    }
+
+    // pascal casing
+    public class Vatandas
+    {
+        public string Ad { get; set; }
+
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
+    }
+
+}
