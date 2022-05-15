837432025a43e7816931044345ca19d63b7559d1
Erol Dalkılıç <eroldalg@gmail.com>
2022-05-16 00:22:42 +0300

    Added

--- kodlama.io/kamplar/Yazılım Geliştirici Yetiştirme Kampı/KampIntro/KampIntro/Program.cs
+++ kodlama.io/kamplar/Yazılım Geliştirici Yetiştirme Kampı/KampIntro/KampIntro/Program.cs
@@ -2,29 +2,33 @@
 
 namespace KampIntro
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            //type safety
-            string kategoriEtiketi = "Kategori";
+
+            //type safety - tip güvenliği
+            //Do not repeat yourself - Kendini tekrarlama
+            //değer tutucu, alias
+
+            string kategoriEtiketi = "Kategoriler";
             int ogrenciSayisi = 32000;
             double faizOrani = 1.45;
-            bool sistemeGirisYapmisMi = false;
-            double dolarDun = 7.45;
-            double dolarBugun = 7.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
 
-            if (dolarDun > dolarBugun)
+            if (dolarDun>dolarBugun)
             {
-                Console.WriteLine("Azalış Butonu");
+                Console.WriteLine("Azalış butonu");
             }
-            else if (dolarDun < dolarBugun)
+            else if (dolarDun<dolarBugun)
             {
-                Console.WriteLine("Artış butonu.");
+                Console.WriteLine("Artış butonu");
             }
             else
             {
-                Console.WriteLine("Eşittir Butonu.");
+                Console.WriteLine("değişmedi butonu");
             }
 
             if (sistemeGirisYapmisMi == true)
@@ -35,8 +39,10 @@ namespace KampIntro
             {
                 Console.WriteLine("Giriş yap butonu");
             }
-
+            
             Console.WriteLine(kategoriEtiketi);
+
+
         }
     }
 }
