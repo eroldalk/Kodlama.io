2ee4d151af8cd9c48ebba515d3a852fb9f3fa297
Erol Dalkılıç <eroldalg@gmail.com>
2022-05-18 20:33:43 +0300

    the end

---
+++ kodlama.io/kamplar/Yazılım Geliştirici Yetiştirme C# Kampı/KampIntro/Dictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            //CreateDictionary createDictionary = new CreateDictionary();
+            //createDictionary.CreateDictionaryMethod();
+
+            MyDictionary<string, int> myDictionaries = new MyDictionary<string, int>();
+
+            myDictionaries.Add("Erol", 19);
+            myDictionaries.Add("Engin", 35);
+
+            for (int i = 0; i < myDictionaries.Values.Length; i++)
+            {
+                Console.WriteLine("Key: {0}, Year of birth: {1}", myDictionaries.Keys[i], myDictionaries.Values[i]);
+            }
+        }
+    }
+}
---
+++ kodlama.io/kamplar/Yazılım Geliştirici Yetiştirme C# Kampı/KampIntro/GameProject/NewEStateUserValidationManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class NewEStateUserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            return true;
+        }
+
+        
+    }
+}
+
+
+
+
+
+
+
